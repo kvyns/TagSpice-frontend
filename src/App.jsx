2dc018fd37d22ea9b2ea73b01504aaeee6886caa
Kavyansh <kd.kavyansh2003@gmail.com>
2024-08-17 23:06:08 +0530

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <LoginPage/>
+    </>
   )
 }

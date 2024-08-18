17264aa190eb2d2382c8b563e41e5865890a3eaf
Kavyansh <kd.kavyansh2003@gmail.com>
2024-08-19 01:16:21 +0530

    Added Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from 'react-router-dom'
+import Layout from './Layout.jsx'
+import LoginPage from './pages/LoginPage/LoginPage.jsx'
+import LandingPage from './pages/LandingPage/LandingPage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<Layout/>}>
+      <Route path = "" element = {<LandingPage/>}/>
+      <Route path = "login" element = {<LoginPage/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+// const [isLogin, setIsLogin] = useState(false)
+import { Link } from 'react-router-dom'
 function LandingPage() {
   return (
     <div>
         <div className='w-full bg-blue-400 left-1/2 m-auto max-w-screen-lg'>
             <div className='font-bold text-center p-5'>
               This is a landing page for TagSpice.
+              <Link to="/login">Login</Link>
             </div>
         </div>
     </div>
